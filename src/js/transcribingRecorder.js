f2064ccac1aad864b702ee25a5e975adeeecc69d
Alan Harnum <aharnum@ocadu.ca>
2017-07-21 17:10:55 -0400

    Basic transcription timing.

--- src/js/transcribingRecorder.js
+++ src/js/transcribingRecorder.js
@@ -9,7 +9,9 @@
         members: {
             template: "<form><button class='sjrk-voiceRecorder-record' type='button'>Record</button>  <button class='sjrk-voiceRecorder-stop' type='button'>Stop</button></form>",
             chunks: [],
-            transcript: []
+            transcriptStart: null,
+            transcript: [],
+            interimTranscript: []
         },
         listeners: {
             "onCreate.initializeRecording": {
@@ -54,11 +56,12 @@
         );
         that.speechRecognition = new webkitSpeechRecognition();
         that.speechRecognition.continuous = true;
+        that.speechRecognition.interimResults = true;
     };
 
     sjrk.voiceRecorder.bindControls = function (that) {
         console.log("bindControls");
-        fluid.each(["record", "pause", "stop"], function(controlName) {
+        fluid.each(["record", "stop"], function(controlName) {
 
             var button = that.locate(controlName);
             button.click(function () {
@@ -70,6 +73,10 @@
     sjrk.voiceRecorder.record = function (that) {
         console.log("Record");
         that.mediaRecorder.start();
+
+        var date = new Date();
+        date = date.toUTCString();
+        that.transcriptStart = date;
 
         var recogEvents = ["onaudiostart", "onaudioend", "onsoundstart", "onsoundend", "onspeechstart", "onspeechend"];
 
@@ -83,40 +90,65 @@
 
         that.speechRecognition.onresult =
         (function (e) {
+
+            var currentTime = new Date();
+            var transcriptStart = new Date(that.transcriptStart);
+            var timeFromStart = (currentTime.getTime() - transcriptStart.getTime()) / 1000;
+
             var transcriptArrayLength = that.transcript.length;
-            console.log(transcriptArrayLength, e.results);
+
+            var isFinal = e.results[transcriptArrayLength].isFinal;
             var transcript = e.results[transcriptArrayLength][0].transcript;
-            that.transcript.push(transcript);
+            if(!isFinal) {
+                if(! that.interimTranscript[transcriptArrayLength]) {
+                    that.interimTranscript[transcriptArrayLength] = {start: timeFromStart, transcript: transcript};
+                }
+                console.log(that.interimTranscript);
+            }
+            if(isFinal) {
+                that.transcript.push({end: timeFromStart, transcript: transcript});
             console.log(transcript);
+            }
+
         });
 
         that.speechRecognition.start();
-        console.log(that.mediaRecorder.state);
     };
 
     sjrk.voiceRecorder.stop = function (that) {
         console.log("Stop");
-        console.log(that.speechRecognition);
 
         that.mediaRecorder.ondataavailable = function (e) {
             that.chunks.push(e.data);
             var blob = new Blob(that.chunks, { 'type' : 'audio/ogg; codecs=opus' });
             that.chunks = [];
-            console.log(blob);
             var audioURL = window.URL.createObjectURL(blob);
-            console.log(audioURL);
             var playerMarkup = "<audio controls src='" + audioURL + "'></audio>";
 
-            var displayedTranscript = that.transcript.join(" ");
-            that.transcript = [];
-            var transcriptMarkup = "<p>" + displayedTranscript + "</p>";
+            var displayedTranscript = [];
+            fluid.each(that.transcript, function (transcriptItem, idx) {
+                var templateValues = {
+                    transcript: transcriptItem.transcript,
+                    end: transcriptItem.end,
+                    start: that.interimTranscript[idx].start
+                };
+
+                var transcriptLine = fluid.stringTemplate("%start - %end %transcript", templateValues);
+                displayedTranscript.push(transcriptLine);
+            });
+
+            var transcriptMarkup = "<p>" + displayedTranscript.join("<br/>") + "</p>";
             that.container.append(playerMarkup);
+            that.container.append("<h2>" + that.transcriptStart + "</h2>");
             that.container.append(transcriptMarkup);
             that.container.append("<hr />");
+            that.transcript = [];
+            that.interimTranscript = [];
+            that.transcriptStart = null;
+            console.log(that);
         };
         that.mediaRecorder.stop();
         that.speechRecognition.stop();
-        console.log(that.mediaRecorder.state);
     };
 
 })(jQuery, fluid);
